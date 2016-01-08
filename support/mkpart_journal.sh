66dcaeaece7535c1fbf126588de91b163127d6e4
wadeholler <wade.holler@gmail.com>
2016-01-07 22:17:05 -0500

    update limits.conf

--- support/mkpart_journal.sh
+++ support/mkpart_journal.sh
@@ -1,20 +1,20 @@
 parted -s -a optimal /dev/$1 mklabel gpt
 parted -s -a optimal /dev/$1 mkpart osd-device-0-journal 0% 74G
-parted -s -a optimal /dev/$1 mkpart osd-device-1-journal 74G 148G
-parted -s -a optimal /dev/$1 mkpart osd-device-2-journal 148G 222G
-parted -s -a optimal /dev/$1 mkpart osd-device-3-journal 222G 296G
-parted -s -a optimal /dev/$1 mkpart osd-device-4-journal 296G 370G
+parted -s -a optimal /dev/$1 mkpart osd-device-3-journal 74G 148G
+parted -s -a optimal /dev/$1 mkpart osd-device-6-journal 148G 222G
+parted -s -a optimal /dev/$1 mkpart osd-device-9-journal 222G 296G
+parted -s -a optimal /dev/$1 mkpart osd-device-12-journal 296G 370G
 
 parted -s -a optimal /dev/$2 mklabel gpt
-parted -s -a optimal /dev/$2 mkpart osd-device-5-journal 0% 74G
-parted -s -a optimal /dev/$2 mkpart osd-device-6-journal 74G 148G
+parted -s -a optimal /dev/$2 mkpart osd-device-1-journal 0% 74G
+parted -s -a optimal /dev/$2 mkpart osd-device-4-journal 74G 148G
 parted -s -a optimal /dev/$2 mkpart osd-device-7-journal 148G 222G
-parted -s -a optimal /dev/$2 mkpart osd-device-8-journal 222G 296G
-parted -s -a optimal /dev/$2 mkpart osd-device-9-journal 296G 370G
+parted -s -a optimal /dev/$2 mkpart osd-device-10-journal 222G 296G
+parted -s -a optimal /dev/$2 mkpart osd-device-13-journal 296G 370G
 
 parted -s -a optimal /dev/$3 mklabel gpt
-parted -s -a optimal /dev/$3 mkpart osd-device-10-journal 0% 74G
-parted -s -a optimal /dev/$3 mkpart osd-device-11-journal 74G 148G
-parted -s -a optimal /dev/$3 mkpart osd-device-12-journal 148G 222G
-parted -s -a optimal /dev/$3 mkpart osd-device-13-journal 222G 296G
+parted -s -a optimal /dev/$3 mkpart osd-device-2-journal 0% 74G
+parted -s -a optimal /dev/$3 mkpart osd-device-5-journal 74G 148G
+parted -s -a optimal /dev/$3 mkpart osd-device-8-journal 148G 222G
+parted -s -a optimal /dev/$3 mkpart osd-device-11-journal 222G 296G
 parted -s -a optimal /dev/$3 mkpart osd-device-14-journal 296G 370G
